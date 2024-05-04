adc3c74689b2d6370299e9b2d9a86d078eb6d910
MOdeus <kushwaikar@gmail.com>
2024-05-04 15:25:19 +0530

    added env file and changed endpoints

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -6,7 +6,7 @@ const ItemList = ({ products, isLoggedIn, userid }) => {
   const [favorites, setFavorites] = useState([]);
   const handleAddToFavorites = (productId) => {
     console.log(productId);
-    axios.patch(`http://localhost:5055/api/users/addfavorite/${userid}`, { item: productId })
+    axios.patch(`https://short-ecommerce.vercel.app/api/users/addfavorite/${userid}`, { item: productId })
       .then(response => {
         setFavorites([...favorites, response.data]);
       })
--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -21,7 +21,7 @@ const Signup = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5055/api/users/addUsers', {
+            const response = await fetch('https://short-ecommerce.vercel.app/api/users/addUsers', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'

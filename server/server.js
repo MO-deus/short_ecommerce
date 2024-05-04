64b73b075d8a6a807ae6035c12edd121e3e7fe84
MOdeus <kushwaikar@gmail.com>
2024-05-04 19:26:52 +0530

    cors update to allow every origin endpoint

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from "cors";
 import userRoute from './routes/users.js';
 import productRoute from './routes/products.js';
-import db_connection  from './db/connection.js';
+import db_connection from './db/connection.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,8 +10,13 @@ db_connection();
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 
-app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRoute);
